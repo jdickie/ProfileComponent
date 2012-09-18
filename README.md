61c55205baafffa03fb1d28497fa1b0ca4b29d7a
Grant Dickie <jgrantd@gmail.com>
2012-09-18 16:29:24 -0400

    Committing base rails files

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 ARHU People Project
 ===================
 
+Description
+===========
+===========
+
+Contracted by Kevin Remmell from the English Department to develop an application that tracks activities and other Human-Resources related data for Faculty, Graduate Students, and Staff. 
+
+
+Features
+========
+========
+
+System will have:
+
+* Access Control
+* Taxonomy Tagging
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ArhuPeople::Application.config.secret_token = '55fdd6a38c03f98bbef17b52eba7a50ab829d8b4694d4be9726add4608b5362c9b56494b32b0dd3ab7d168d8e2c0698042c15bac975da0873be3bc0f56f2439d'
